2ee032cc1e180370c1a42de8ed7030fd33496425
Carlos Alberto <lionbllack1@gmail.com>
2024-07-16 22:31:19 -0300

    projeto store

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './Components/Pages/HomePage';
+import './Components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home e Mopa</h1>
+    </div>
+  );
+}
